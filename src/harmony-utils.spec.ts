332294e998f550eda5a7bb1f630d72e6e12bb7a2
Ulf Steinberg <ulf@steinberg-online.de>
2018-06-22 07:22:06 +0200

    fix: HarmonyHub.Press did not work (wrong type and wrong encoding)

--- src/harmony-utils.spec.ts
+++ src/harmony-utils.spec.ts
@@ -1,28 +1,28 @@
-import { test } from 'ava';
+import { test } from 'ava'
 
-import { decodeResponse, encodeRequest } from './harmony-utils';
+import { decodeResponse, encodeRequest } from './harmony-utils'
 
 test('Can encode and decode request', t => {
     const encodedRequest = encodeRequest({
         hello: 'world',
         test: true,
         timestamp: 123,
-    });
+    })
 
-    t.is(encodedRequest, 'hello=world:test=true:timestamp=123');
+    t.is(encodedRequest, 'hello=world:test=true:timestamp=123')
     t.deepEqual(decodeResponse(encodedRequest), {
         hello: 'world',
         test: 'true',
         timestamp: '123',
-    });
-});
+    })
+})
 
 test('Can decode nested replies', t => {
     const input = `serverIdentity=77e2af66-XXXX-4cbb-381a-560289d57d26:
                    hubId=106:identity=77e2af66-XXXX-4cbb-381a-560289d57d26:
                     status=succeeded:protocolVersion={XMPP="1.0", HTTP="1.0", RF="1.0", WEBSOCKET="1.0"}:
                     hubProfiles={Harmony="2.0"}:
-                    productId=Pimento:friendlyName=MyHub`;
+                    productId=Pimento:friendlyName=MyHub`
 
     t.deepEqual(decodeResponse(input), {
         serverIdentity: '77e2af66-XXXX-4cbb-381a-560289d57d26',
@@ -38,21 +38,23 @@ test('Can decode nested replies', t => {
         hubProfiles: { Harmony: '2.0' },
         productId: 'Pimento',
         friendlyName: 'MyHub',
-    });
-});
+    })
+})
 
-test.skip('Can encode nested request', t => {
+test('Can encode nested request', t => {
     const input = {
+        status: 'release',
+        timestamp: 1000,
         action: {
-            type: 'IRCommand',
-            deviceId: '11586428',
-            command: 'VolumeDown',
+            action:
+                '{"command":"Mute","type":"IRCommand","deviceId":"46156292"}',
+            name: 'Mute',
+            label: 'Mute',
         },
-        status: 'press',
-    };
+    }
 
     t.is(
         encodeRequest(input),
-        'action={"type"::"IRCommand","deviceId"::"11586428","command"::"VolumeDown"}:status=press'
-    );
-});
+        'status=release:timestamp=1000:action={"command"::"Mute","type"::"IRCommand","deviceId"::"46156292"}:name=Mute:label=Mute'
+    )
+})
